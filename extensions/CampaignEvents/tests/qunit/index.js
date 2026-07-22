a2b04973592569a8b9eee6df612c6072a281acc6
Aafi <aaqibanjumaafi@gmail.com>
2026-07-22 17:27:11 +0530

    fix: broken submodule

--- extensions/CampaignEvents/tests/qunit/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-require( './ext.campaignEvents.TimeZoneConverter.test.js' );
-require( './ext.campaignEvents.specialPages/eventdetails/EmailManager.test.js' );
-require( './ext.campaignEvents.specialPages/eventdetails/ParticipantsManager.test.js' );
