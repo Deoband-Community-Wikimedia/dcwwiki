469889cc7653da197bb7d9e4bfe38e71cdfa9497
Aafi <aaqibanjumaafi@gmail.com>
2026-08-14 12:00:44 +0530

    Update external image source configuration

--- LocalSettings.php
+++ LocalSettings.php
@@ -655,3 +655,7 @@ $wgMaxImageArea = 100000000; // Increase this to handle larger PNGs
 $wgScribuntoDefaultEngine = 'luastandalone';
 $wgScribuntoEngineConf['luastandalone']['luaPath'] = __DIR__ . '/extensions/Scribunto/includes/Engines/LuaStandalone/binaries/lua5_1_5_linux_64_generic/lua';
 $wgSessionCacheType = CACHE_DB; // Forces stable, database-backed session tracking
+$wgAllowExternalImagesFrom = 'https://diff.wikimedia.org/';
+$wgAllowExternalImagesFrom = [
+    'https://diff.wikimedia.org/'
+];
