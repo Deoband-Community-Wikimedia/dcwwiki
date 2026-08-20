08875c489aa3375b8e049b5112da17630c2872cf
Aafi <aaqibanjumaafi@gmail.com>
2026-08-20 15:45:34 +0530

    Update OAuth settings and user namespace definitions

--- LocalSettings.php
+++ LocalSettings.php
@@ -607,6 +607,11 @@ $wgGroupPermissions['*']['createaccount'] = false;
 $wgGroupPermissions['*']['autocreateaccount'] = true;
 $wgPluggableAuth_EnableLocalLogin = true;
 $wgPluggableAuth_EnableAutoLogin = true;
+$wgExtraNamespaces[NS_USER] = "Member";
+$wgExtraNamespaces[NS_USER_TALK] = "Member_talk";
+$wgOAuthMigrateUsersByUsername = true; 
+$wgOAuthDisallowRemoteOnlyAccounts = false;
+
 $wgPluggableAuth_Config['Deoband Community Wikimedia'] = [
     'plugin' => 'WSOAuth',
     'data' => [
@@ -614,15 +619,9 @@ $wgPluggableAuth_Config['Deoband Community Wikimedia'] = [
         'uri' => 'https://meta.wikimedia.org/w/index.php?title=Special:OAuth',
         'clientId' => $_ENV['MW_OAUTH_CLIENT_ID'] ?? '',
         'clientSecret' => $_ENV['MW_OAUTH_CLIENT_SECRET'] ?? '',
-		'redirectUri' => 'https://dcwwiki.org',
     ],
     'buttonLabelMessage' => 'dcw-mw-login-button-label'
 ];
-$wgOAuthMigrateUsersByUsername = true; 
-$wgOAuthDisallowRemoteOnlyAccounts = false;
-$wgExtraNamespaces[NS_USER] = "Member";
-$wgExtraNamespaces[NS_USER_TALK] = "Member_talk";
-
 /*
 
 wfLoadExtension( 'ConfirmAccount' );
